ae37bf0feca6c6bb55dcbe1899c113e4046872a4
Jordan Fowler <54287603+JordanFowler22@users.noreply.github.com>
2020-05-18 14:44:12 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 class Calculator {
-    
+
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
-        this,currentOperandTextElement = currentOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
         this.clear
     }
 
@@ -26,7 +26,7 @@ class Calculator {
         if (this.previousOperand !== '') {
             this.compute()
         }
-        this.currentOperand = operation
+        this.operation = operation
         this.previousOperand = this.currentOperand
         this.currentOperand = ''
     }
@@ -38,19 +38,19 @@ class Calculator {
         if (isNaN(prev) || isNaN(current)) return
         switch (this.operation) {
             case '+':
-             computation = prev + current 
-             break
+                computation = prev + current
+                break
             case '-':
-             computation = prev - current 
-             break
+                computation = prev - current
+                break
             case '*':
-             computation = prev * current 
-             break
-            case '/':
-             computation = prev / current 
-             break
+                computation = prev * current
+                break
+            case '÷':
+                computation = prev / current
+                break
             default:
-                return 
+                return
         }
         this.currentOperand = computation
         this.operation = undefined
@@ -65,7 +65,7 @@ class Calculator {
         if (isNaN(integerDigits)) {
             integerDisplay = ''
         } else {
-            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0 })
+            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
         }
         if (decimalDigits != null) {
             return `${integerDisplay}.${decimalDigits}`
@@ -77,22 +77,22 @@ class Calculator {
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
         if (this.operation != null) {
-            this.previousOperandTextElement.innerText = 
-             `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+            this.previousOperandTextElement.innerText =
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
         } else {
             this.previousOperandTextElement.innerText = ''
-        }      
+        }
     }
 }
 
 
 const numberButtons = document.querySelectorAll('[data-number')
 const operationButtons = document.querySelectorAll('[data-operation')
-const equalsButton = document.querySelectorAll('[data-equals]')
-const deleteButton = document.querySelectorAll('[data-delete]')
-const allClearButton = document.querySelectorAll('[data-all-clear]')
-const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')
-const currentOperandTextElement = document.querySelectorAll('[data-current-operand]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
@@ -124,5 +124,3 @@ deleteButton.addEventListener('click', button => {
     calculator.delete()
     calculator.updateDisplay()
 })
-
-
